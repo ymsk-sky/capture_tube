30242d2e6703caa5ca00c7861b2288b51dbf8cb2
ymsk.sky <ymsk.sky.95@gmail.com>
2020-02-04 22:18:31 +0900

    グレイスケール化

--- test.py
+++ test.py
@@ -1,25 +1,14 @@
 # -*- utf-8 -*-
 
-import time
 import cv2
 import pafy
 import youtube_dl
-import pyocr
-from PIL import Image
 
 def main():
-    # OCRツール(Tesseractを使う)
-    tools = pyocr.get_available_tools()
-    if len(tools) == 0:
-        print('no tools')
-        return
-    tool = tools[0]
-
     # Youtubeから動画を読み込む
     url = 'https://www.youtube.com/watch?v=bojIHQKjCwo'
     v_pafy = pafy.new(url)
     play = v_pafy.getbest(preftype="webm")
-    time.sleep(3)
     video = cv2.VideoCapture(play.url)
     if not video.isOpened():
         return
@@ -36,13 +25,7 @@ def main():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ret, binary = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
 
-        # 文字認識
-        txt = tool.image_to_string(image=Image.fromarray(binary),
-                                   lang='jpn',
-                                   builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-        print(txt)
-
-        cv2.imshow('tube', frame)
+        cv2.imshow('tube', binary)
 
         key = cv2.waitKey(fps) & 0xFF
         if key == ord('q'):
