c21bc63155573c488722af2052544530ca4a818e
ymsk.sky <ymsk.sky.95@gmail.com>
2020-02-29 18:16:56 +0900

    輪郭抽出実装

--- contours.py
+++ contours.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import sys
 
 video = cv2.VideoCapture('test.mp4')
 if not video.isOpened():
-    return
+    sys.exit()
 
 fps = int(video.get(cv2.CAP_PROP_FPS))
 
@@ -14,9 +15,20 @@ while video.isOpened():
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, binary = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY_INV)
 
     # 境界検出
-    contours = cv2.findContours()
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cont in contours:
+        x, y, w, h = cv2.boundingRect(cont)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('', frame)
+
+    key = cv2.waitKey(fps)
+    if key == ord('q'):
+        break
 
 video.release()
 cv2.destroyAllWindows()
