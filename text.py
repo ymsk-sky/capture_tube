74ed84d4efe6e687caac3a8496eb2eb64fa1ee8b
ymsk.sky <ymsk.sky.95@gmail.com>
2020-03-05 22:57:10 +0900

    implement text detection py script

--- text.py
+++ text.py
@@ -2,6 +2,10 @@
 
 import cv2
 import sys
+import numpy as np
+
+nm1 = 'trained_classifierNM1.xml'
+nm2 = 'trained_classifierNM2.xml'
 
 src = 'test.mp4'
 video = cv2.VideoCapture(src)
@@ -15,7 +19,28 @@ while video.isOpened():
     if not ret:
         break
 
-    pass
+    channels = cv2.text.computeNMChannels(frame)
+    cn = len(channels) - 1
+    for c in range(cn):
+        channels.append((255 - channels[c]))
+
+    for channel in channels:
+        erc1 = cv2.text.loadClassifierNM1(nm1)
+        er1 = cv2.text.createERFilterNM1(erc1, 16, 0.00015, 0.13, 0.2, True, 0.1)
+
+        erc2 = cv2.text.loadClassifierNM2(nm2)
+        er2 = cv2.text.createERFilterNM2(erc2, 0.5)
+
+        regions = cv2.text.detectRegions(channel, er1, er2)
+
+        rects = cv2.text.erGrouping(frame, channel, [r.tolist() for r in regions])
+
+        for r in range(np.shape(rects)[0]):
+            rect = rects[r]
+            cv2.rectangle(frame, (rect[0], rect[1]), (rect[0]+rect[2], rect[1]+rect[3]), (0, 0, 0), 2)
+            cv2.rectangle(frame, (rect[0], rect[1]), (rect[0]+rect[2], rect[1]+rect[3]), (255, 255, 255), 1)
+
+    cv2.imshow('Text detection', frame)
 
     key = cv2.waitKey(fps)
     if key == ord('q'):
